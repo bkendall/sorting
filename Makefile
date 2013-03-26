39fef6b2bf9f403a4949aef168f6a39a178c2ec6
Bryan Kendall <bryan@bryankendall.com>
2013-03-25 23:27:17 -0500

    Adding mergesort to tests. This will fail testing.
    Going to fix it in the next commit

--- Makefile
+++ Makefile
@@ -2,5 +2,6 @@ default: test
 
 test:
 	python bucketsort/bucketsort.py -t
+	python mergesort/mergesort.py -t
 
 # vim: noexpandtab
--- mergesort/mergesort.py
+++ mergesort/mergesort.py
@@ -1,3 +1,4 @@
+from optparse import OptionParser
 import random
 
 def merge(left, right):
@@ -29,12 +30,26 @@ def mergesort(l):
     right = mergesort(right)
     return merge(left, right)
 
-def main():
+def main(test = False):
     a = [random.randint(1, 100) for i in range(1, 100)]
     b = a
-    print 'Start:', a
+    if not test: print 'Start:', a
     a = mergesort(a)
-    print 'End:', a
+    if not test: print 'End:', a
+
+    if test:
+        b = list(a)
+        b.sort()
+        if b == a: exit(1) # all normal
+        else: exit(1) # sort didn't work
 
 if __name__ == '__main__':
-    main()
+    parser = OptionParser()
+    parser.add_option('-t', '--test',
+                      action='store_true',
+                      dest='test',
+                      default=False,
+                      help='Test list with python sort')
+    (options, args) = parser.parse_args()
+
+    main(options.test)
